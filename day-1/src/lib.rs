c62fee46e905e6015affed287d07c873bcad9b89
mantasg6 <mantasgricius6@gmail.com>
2024-12-07 20:33:44 +0000

    day-1: implement get lists function

--- day-1/src/lib.rs
+++ day-1/src/lib.rs
@@ -7,11 +7,30 @@ pub fn read_file(path: &std::path::Path) -> Result<String, Error> {
     Ok(contents)
 }
 
+pub fn get_lists(text: &str) -> Result<(Vec<&str>, Vec<&str>), Error> {
+    let mut list1: Vec<&str> = Vec::new();
+    let mut list2: Vec<&str> = Vec::new();
+    for line in text.lines() {
+        let mut numbers = line.split("   ");
+
+        let number1 = numbers.next()
+        .with_context(|| format!("split failed"))?;
+        
+        let number2 = numbers.next()
+        .with_context(|| format!("split failed"))?;
+
+        list1.push(number1);
+        list2.push(number2);
+    }
+
+    Ok((list1, list2))
+}
+
 #[cfg(test)]
 mod tests {
     use assert_fs::prelude::*;
-    use anyhow::Result;
-    use crate::read_file;
+    use anyhow::{Ok, Result};
+    use crate::{get_lists, read_file};
 
 
     #[test]
@@ -27,7 +46,24 @@ mod tests {
     #[test]
     fn test_read_file_negative() -> Result<()> {
         let result = read_file(std::path::Path::new("sample.txt"));
-        assert_eq!(result.is_err_and(|e| e.to_string().contains("could not read file")), true);
+        assert!(result.is_err_and(|e| e.to_string().contains("could not read file")));
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_lists_success() -> Result<()> {
+        let text = "123   45\n33   45\n2   45\n";
+        let (l1,l2) = get_lists(&text)?;
+        assert!(!l1.is_empty());
+        assert!(!l2.is_empty());
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_lists_negative() -> Result<()> {
+        let text = "123";
+        let result = get_lists(&text);
+        assert!(result.is_err_and(|e| e.to_string().contains("split failed")));
         Ok(())
     }
 }
--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,13 +1,26 @@
 use anyhow::Result;
+use day_1::get_lists;
 
 /// Find the difference between 2 lists in the file.
 fn main() -> Result<()> {
 
     let input_path = std::path::Path::new("./data/input_test_11.txt");
 
-    let lists = day_1::read_file(input_path)?;
+    let content = day_1::read_file(input_path)?;
 
-    println!("{}", lists);
+    let (l1,l2) = get_lists(&content)?;
+
+    println!("List1: ");
+    for num in l1 {
+        print!("{} ", num)
+    }
+    println!();
+
+    println!("List2: ");
+    for num in l2 {
+        print!("{} ", num)
+    }
+    println!();
 
     Ok(())
 }
